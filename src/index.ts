9ed96881c7600441d5547c2fb83f9d74b3664876
Frédéric Collonval <fcollonval@gmail.com>
2022-01-11 17:15:47 +0100

    First version

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the quetz-theme extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'quetz-theme',
+  requires: [IThemeManager],
+  autoStart: true,
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    const style = 'quetz-theme/index.css';
+
+    manager.register({
+      name: 'Quetz',
+      isLight: false,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  }
+};
+
+export default extension;
