424d1c3c9e557e7f55746ae454e72c23b291be74
Frédéric Collonval <fcollonval@gmail.com>
2022-01-27 17:24:51 +0100

    Use lumino application directly

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,22 @@
-import type {
-  JupyterFrontEnd,
-  JupyterFrontEndPlugin,
-} from '@jupyterlab/application';
+import type { IPlugin } from '@lumino/application';
 
 import { IThemeManager } from '@jupyterlab/apputils';
 
 /**
  * Initialization data for the quetz-theme extension.
  */
-const extension: JupyterFrontEndPlugin<void> = {
+const extension: IPlugin<any, void> = {
   id: 'quetz-theme',
   requires: [IThemeManager],
   autoStart: true,
-  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+  activate: (app: any, manager: IThemeManager) => {
     const style = 'quetz-theme/index.css';
 
     manager.register({
       name: 'Quetz',
       isLight: false,
       load: () => manager.loadCSS(style),
-      unload: () => Promise.resolve(undefined),
+      unload: () => Promise.resolve(),
     });
   },
 };
